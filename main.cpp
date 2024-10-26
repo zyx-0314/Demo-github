0305aea84214a7d2a3eb9df07e45956743a2c68a
zyx-0314 <ramirezian037@gmail.com>
2024-10-26 14:51:41 +0800

    added a correct cin

--- main.cpp
+++ main.cpp
@@ -5,9 +5,12 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
+    string response;
+    
     cout 
          << "Hello world\n"
          << "Hi!";
+    cin >> response;
     
     system("PAUSE");
     return EXIT_SUCCESS;
