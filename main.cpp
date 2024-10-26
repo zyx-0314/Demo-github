39199036c9888a2873ab3d96222b431f3ef8abe8
zyx-0314 <ramirezian037@gmail.com>
2024-10-26 14:40:39 +0800

    initialized c++ project

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    cout << "Hello world";
+    
+    system("PAUSE");
+    return EXIT_SUCCESS;
+}
